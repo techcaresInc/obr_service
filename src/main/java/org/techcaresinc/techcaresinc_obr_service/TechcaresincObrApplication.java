360548721b156ff48bb07a97323fcb53bbe9d6bd
JMchfundi <akidamjaffar@gmail.com>
2019-12-23 10:11:01 +0300

    okay

--- src/main/java/org/techcaresinc/techcaresinc_obr_service/TechcaresincObrApplication.java
+++ src/main/java/org/techcaresinc/techcaresinc_obr_service/TechcaresincObrApplication.java
@@ -4,17 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class TechcaresincObrApplication extends SpringBootServletInitializer {
 
-	/* techcares inc - a solution to your worries, deployment to external server*/
-	/* comment the SpringApplicationBuilder for running test without deploy to external server*/
+/*
+	*/
+/* techcares inc - a solution to your worries, deployment to external server*//*
+
+	*/
+/* comment the SpringApplicationBuilder for running test without deploy to external server*//*
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(TechcaresincObrApplication.class);
 	}
+*/
 
+	@RequestMapping (value = "/")
+	public String root (Model model) {
+		return "index";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TechcaresincObrApplication.class, args);
 	}
